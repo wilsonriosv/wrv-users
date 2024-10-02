c3cc332e143358bae053c929bf812adab6f09380
wilsonriosv <wilsonriosv@gmail.com>
2024-10-02 12:06:53 -0500

    Cargue inicial. Cambios en App.tsx

--- App.tsx
+++ App.tsx
@@ -4,11 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hola mundo desde app usuarios</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
